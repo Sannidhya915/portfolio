de3a1fa31a0c86210817a8f392d2aecdec87a8f9
Sannidhya Sahu <sannidhyasahu915@gmail.com>
2024-03-19 01:40:33 +0530

    Working on landing page, nav done

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// -- ---------- SHOW MENU ---------- --
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+//----- MENU SHOW -----
+/* Validate if constant exists */
+if(navToggle){
+        navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+//----- MENU HIDDEN -----
+/* Validate if constant exists */ 
+if(navClose){
+        navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// -- ---------- REMOVE MENU MOBILE ---------- --
+
+// -- ---------- ADD BLUR TO HEADER ---------- --
+
+// -- ---------- EMAIL JS ---------- --
+
+// -- ---------- SHOW SCROLL UP ---------- --
+
+// -- ---------- SCROLL SECTIONS ACTIVE LINK ---------- --
+
+// -- ---------- SCROLL REVEAL ANIMATION ---------- --
